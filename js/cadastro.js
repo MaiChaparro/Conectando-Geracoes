9963723acd58be6ac9b70334043637aa1389d664
MaiChaparro <maida-alejandra@outlook.com>
2026-07-27 19:40:26 -0300

    telas Voluntários finalizadas

---
+++ js/cadastro.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const formCadastro = document.querySelector('.form-cadastro');
+    const seletorPessoa = document.getElementById('pessoa');
+
+    if (formCadastro) {
+        formCadastro.addEventListener('submit', (event) => {
+            
+            event.preventDefault();
+
+            const tipoPessoa = seletorPessoa.value;
+
+            
+            if (tipoPessoa === "voluntario") {
+               
+               if (tipoPessoa === "voluntario") {
+              window.location.href = "./telaVoluntario.html"; 
+}
+            } else if (tipoPessoa === "beneficiario") {
+              
+                window.location.href = "./telaInicialIdoso.html"; 
+            } else {
+                alert("Por favor, selecione se você é Voluntário ou Beneficiário.");
+            }
+        });
+    }
+});
---
+++ js/carrosselVol.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    
+    const btnEsquerda = document.getElementById('btn-esquerda');
+    const btnDireita = document.getElementById('btn-direita');
+    const listaMissoes = document.getElementById('lista-missoes');
+
+ 
+    const distanciaRolagem = 260; 
+
+    
+    btnDireita.addEventListener('click', () => {
+        listaMissoes.scrollBy({ 
+            left: distanciaRolagem, 
+            behavior: 'smooth' 
+        });
+    });
+
+    btnEsquerda.addEventListener('click', () => {
+        listaMissoes.scrollBy({ 
+            left: -distanciaRolagem, 
+            behavior: 'smooth' 
+        });
+    });
+});
